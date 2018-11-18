9765033cfee4dbc478a14272ba1362eee4750680
Will Mann <william@mannclan.net>
2018-11-18 21:22:58 +0000

    Added Tables

---
+++ src/components/LeaguesTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+export default class LeaguesTable extends React.Component{
+    render(){
+        return (
+            <Table>
+
+            </Table>
+        );
+    }
+}
